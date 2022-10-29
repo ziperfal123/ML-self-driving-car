d0d5914e2467ec44e3b8159b9ad728af288b6e80
ziperfal <ziperfal123@gmail.com>
2022-10-29 16:37:51 +0300

    add edge lines

--- main.js
+++ main.js
@@ -3,6 +3,7 @@ const canvas = document.getElementById("myCanvas")
 canvas.width = 200
 
 const ctx = canvas.getContext("2d")
+const road = new Road(canvas.width / 2, canvas.width)
 const car = new Car(100,100, 30, 50)
 
 
@@ -12,6 +13,7 @@ function animate() {
     car.update()
 
     canvas.height = window.innerHeight
+    road.draw(ctx)
     car.draw(ctx)
 
     requestAnimationFrame(animate)
