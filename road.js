91a98a0a00844a5b1100a14068be2ad594ad21f8
ziperfal <ziperfal123@gmail.com>
2022-11-01 20:58:13 +0200

    draw lanes

--- road.js
+++ road.js
@@ -12,18 +12,28 @@ class Road {
         this.bottom = infinity
     }
 
+    getLaneCenter(laneIndex) {
+        const laneWidth = this.width / this.laneCount
+        return this.left + laneWidth / 2 + Math.min(laneIndex, this.laneCount - 1)  * laneWidth
+
+    }
+
     draw(ctx) {
         ctx.lineWidth = 5
         ctx.strokeStyle = "white"
+        for(let i = 0 ; i <= this.laneCount ; i++) {
+            const x = lerp(this.left, this.right, i / this.laneCount)
 
-        ctx.beginPath()
-        ctx.moveTo(this.left , this.top)
-        ctx.lineTo(this.left , this.bottom)
-        ctx.stroke()
+            if(i > 0 && i < this.laneCount) {
+                ctx.setLineDash([20,20])
+            } else {
+                ctx.setLineDash([0, 0])
+            }
 
-        ctx.beginPath()
-        ctx.moveTo(this.right , this.top)
-        ctx.lineTo(this.right , this.bottom)
-        ctx.stroke()
+            ctx.beginPath()
+            ctx.moveTo(x , this.top)
+            ctx.lineTo(x , this.bottom)
+            ctx.stroke()
+        }
     }
 }
